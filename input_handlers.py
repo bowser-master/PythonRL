7e5d02c978d3b9e3bf75daba53fb9f5c6417a231
bowser <bowser.victor@gmail.com>
2018-11-29 16:44:21 -0200

    Adding Numpad Support

--- input_handlers.py
+++ input_handlers.py
@@ -9,14 +9,23 @@ import libtcodpy as lbtc
 
 def handle_keys(key):
     #Movement Keys
-    if key.vk == lbtc.KEY_UP:
+    if key.vk == lbtc.KEY_UP or key.vk == lbtc.KEY_KP8:
         return {'move': (0,-1)}
-    elif key.vk == lbtc.KEY_DOWN:
+    elif key.vk == lbtc.KEY_DOWN or key.vk == lbtc.KEY_KP2:
         return {'move': (0,1)}
-    elif key.vk == lbtc.KEY_LEFT:
+    elif key.vk == lbtc.KEY_LEFT or key.vk == lbtc.KEY_KP4:
         return {'move': (-1,0)}
-    elif key.vk == lbtc.KEY_RIGHT:
+    elif key.vk == lbtc.KEY_RIGHT or key.vk == lbtc.KEY_KP6:
         return {'move': (1,0)}
+        #Diagonals
+    elif key.vk == lbtc.KEY_KP3:
+        return {'move': (1,1)}
+    elif key.vk == lbtc.KEY_KP9:
+        return {'move': (1,-1)}
+    elif key.vk == lbtc.KEY_KP1:
+        return {'move': (-1,1)}
+    elif key.vk == lbtc.KEY_KP7:
+        return {'move': (-1,-1)}
 
     if key.vk == lbtc.KEY_ENTER and key.lalt:
         #Alt+Enter: toggle full screen
